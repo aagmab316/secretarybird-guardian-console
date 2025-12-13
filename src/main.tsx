7dd841584fb4867496ee49396410233861dabf13
A 316 <aagmab316@users.noreply.github.com>
2025-12-13 10:54:57 -0500

    test: intentional lint error to prove CI gate

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 
+// INTENTIONAL LINT ERROR: unused variable to test CI gate
+const unusedVariable = "This should fail lint";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
